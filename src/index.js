6854529fef43ba6f87479ee340fcbc6a8b012c50
CamilaVerso <camilaverso2@gmail.com>
2023-04-24 21:45:11 -0300

    Primeiras modificações

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+function ocultarCartao(cartao) {
+	let escondeNum = [];
+		for(i = 0; i < cartao.length; i++){
+			if(i < cartao.length-4){
+				escondeNum.push("*");
+		}else{
+			escondeNum.push(cartao[i]);
+
+		}
+
+	}
+	return escondeNum.join("");
+}
