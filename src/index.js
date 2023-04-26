32196143b016012cb5f246b6b7b5108959e4d7da
CamilaVerso <camilaverso2@gmail.com>
2023-04-25 22:06:21 -0300

    Html organizado e função de esconder o número do cartão

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
 import validator from './validator.js';
 
 console.log(validator);
-
-function ocultarCartao(cartao) {
-	let escondeNum = [];
-		for(i = 0; i < cartao.length; i++){
-			if(i < cartao.length-4){
-				escondeNum.push("*");
-		}else{
-			escondeNum.push(cartao[i]);
-
-		}
-
-	}
-	return escondeNum.join("");
-}
--- src/validator.js
+++ src/validator.js
@@ -2,4 +2,19 @@ const validator = {
   // ...
 };
 
+function ocultarCartao(cartao) {
+  const escondeNum = [];
+  for( let i = 0; i < cartao.length; i++){
+    if(i < cartao.length-4){
+      escondeNum.push("#");
+    }else{
+      escondeNum.push(cartao[i]);
+
+    }
+
+  }
+  return escondeNum.join("");
+}
+ 
 export default validator;
+
