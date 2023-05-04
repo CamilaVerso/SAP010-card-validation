10b903003f73c3590dad56bc7e08257dabd21731
CamilaVerso <camilaverso2@gmail.com>
2023-05-04 19:21:18 -0300

    Refiz o HTML e fiz a parte do JavaScript

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+const botaoEnviar = document.getElementById("enviar");
+
+botaoEnviar.addEventListener("click", verificar);
+function verificar() {
+  const numCartao = document.getElementById("numeroCartao").value;
+
+  const resultado = validator.isValid(numCartao);
+  const maskifyCartao = validator.maskify(numCartao);
+
+  if (numCartao === "") {
+    alert("Favor inserir o número do cartão.");
+  } else if (resultado === true) {
+    alert(maskifyCartao + " é válido!");
+  } else {
+    alert(maskifyCartao + " é inválido!");
+  }
+}
--- src/validator.js
+++ src/validator.js
@@ -1,20 +1,33 @@
 const validator = {
-  // ...
-};
+  isValid(creditCardNumber) {
+    const reverseCard = creditCardNumber.split("").reverse();
 
-function ocultarCartao(cartao) {
-  const escondeNum = [];
-  for( let i = 0; i < cartao.length; i++){
-    if(i < cartao.length-4){
-      escondeNum.push("#");
-    }else{
-      escondeNum.push(cartao[i]);
+    let addNumbers = 0;
 
+    for (let i = 0; i < reverseCard.length; i++) {
+      let numberCardValidater = parseInt(reverseCard[i]);
+
+      if (i % 2 === 1) {
+        numberCardValidater = numberCardValidater * 2;
+        if (numberCardValidater > 9) {
+          numberCardValidater = numberCardValidater - 9;
+        }
+      }
+
+      addNumbers = addNumbers + numberCardValidater;
     }
 
-  }
-  return escondeNum.join("");
-}
- 
-export default validator;
+    if (addNumbers % 10 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  },
 
+  maskify(creditCardNumber) {
+    const numMask = creditCardNumber.replace(/.(?=.{4})/g, "#");
+
+    return numMask;
+  },
+};
+export default validator;
